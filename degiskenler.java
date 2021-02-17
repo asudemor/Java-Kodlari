214ec48bcaf06269f0b2b76a52d2d0ced2389225
asudemor <45600400+asudemor@users.noreply.github.com>
2021-02-17 15:38:13 +0300

    Add files via upload

---
+++ degiskenler.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+		double sayi = 12.5;
+		sayi = -125;
+		char karakter = 'A';
+		boolean dogruMu = false;
+		
+		System.out.println(mesaj + ogrenciSayisi);
+		
+  }
+}
